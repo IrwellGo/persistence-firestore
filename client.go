193d702df75f6d60a68ba975e09dd4c6cfef4c8a
edjfletcher <ed@>
2023-02-17 16:58:17 +0000

    added client with create method

---
+++ client.go
@@ -0,0 +1,41 @@
+package persistence_firestore
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+)
+
+type Client struct {
+	client     *firestore.Client
+	collection string
+}
+
+func New(client *firestore.Client, collection string) Client {
+	return Client{
+		client,
+		collection,
+	}
+}
+
+func NewSimplified(ctx context.Context, projectId string, collection string) (Client, error) {
+	client, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		return Client{}, err
+	}
+
+	return New(client, collection), nil
+}
+
+func (r Client) Create(ctx context.Context, id string, object interface{}) error {
+	_, err := r.getDocRef(id).Create(ctx, object)
+
+	return err
+}
+
+func (r Client) getCollectionRef() *firestore.CollectionRef {
+	return r.client.Collection(r.collection)
+}
+
+func (r Client) getDocRef(id string) *firestore.DocumentRef {
+	return r.getCollectionRef().Doc(id)
+}
